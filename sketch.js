1576bc8cd98cf652b03572bfa48c21a780593bbb
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2025-01-30 08:15:58 -0800

    cleaned up readme and starter code.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
 // Global Variables
 let winStatement = "";
 let playerChoice = 0; 
@@ -17,6 +18,7 @@ let QUESTION;
 let ROCK;
 
 
+
 function preload(){
     PC = loadImage('img/computer.jpg');
     QUESTION = loadImage('img/question.jpg');
@@ -24,7 +26,6 @@ function preload(){
     // TODO find and load images for paper and    
     // scissors.
 
-    
 }
 
 function setup() {
@@ -42,7 +43,7 @@ function displayChoice(choice, x, y){
     // TODO Finish the displayChoice function  
     // for the other options
 
-    
+
 }
 
 function draw() {
@@ -52,46 +53,45 @@ function draw() {
 
     // Call displayChoice for the player
     displayChoice(playerChoice, 100, 150);
+
     // Call displayChoice for the computer
     displayChoice(computerChoice, 350, 150);
-    
+
 
     determineWinner(playerChoice, computerChoice);
     text(winStatement, 50, 350);
 }
 
 function keyPressed(){
-    // TODO Use selection to set the playerChoice
-    // based on which key the user pressed.
-    // to 1 for rock, 2 for paper or 3 for 
-    // scissors.
+    // TODO Use selection to set the playerChoice based on which 
+    // key the user pressed. 1 for rock, 2 for paper, or 3 for scissors.
 
 
 
-    
 
-    // TODO generate a random selection for 
-    // the computerChoice.
-    
-    
+
+    // TODO generate a random selection for the computerChoice.
+
+
 }
 
 function determineWinner(player, comp){
-    // Use conitional statements to generate a
+    // Use conditional statements to generate a
     // statement about who won.
     // Example "You win, rock crushes scissors"
     if( player < 1 || comp < 1){
         winStatement = "No winner yet."
     }
+
+
     // TODO Finish the determineWinner function
-    // It should compare player then update
+    // It should compare playerChoice to the computerChoice then update
     // winStatement appropriately
-    // Remember 1 is rock, 2 is paper and 
-    // 3 is scissors
+    // Remember 1 is rock, 2 is paper and 3 is scissors
     // The || symbol means OR
     // The && symbol means AND
 
 
-    
-        
+
+
 }
